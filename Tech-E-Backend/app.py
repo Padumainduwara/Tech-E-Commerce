3d1b34d7b3d415d5e94950f98b847d87f432b47d
Paduma Induwara <induwarapaduma2002@gmail.com>
2024-11-19 21:34:57 +0530

    modified session

--- Tech-E-Backend/app.py
+++ Tech-E-Backend/app.py
@@ -1,13 +1,21 @@
 from flask import Flask
+from flask_session import Session
 from user import user_blueprint
 from flask_cors import CORS
+from datetime import timedelta
 
 app = Flask(__name__)
 
-# Set a secure secret key for session handling
+# Set a secret key for session
 app.secret_key = 'your_secret_key'
 
-# Enable CORS with support for credentials
+# Flask-Session configuration
+app.config['SESSION_TYPE'] = 'filesystem'  # Store session data in files
+app.config['SESSION_PERMANENT'] = True  # Persistent session
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=30)  # Set session lifetime to 30 days
+Session(app)
+
+# Enable CORS
 CORS(app, supports_credentials=True)
 
 # Register the user blueprint
